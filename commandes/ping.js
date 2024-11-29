c66a5712c38a3641128aab8612f72a32b879c4ab
Ibrahimusaiidy <saiidibrahim192@gmail.com>
2024-11-29 13:43:50 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "DULLAH✌️MD 💥 respond speed is 30000000009 m/s ";
+    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "Mr b² 💥 respond speed is 000007🔥m/s ";
     let d = '                                                                         add more status✌️';
     let varmess = z + d;
     var mp4 = 'https://files.catbox.moe/jq3oin.mp4';
