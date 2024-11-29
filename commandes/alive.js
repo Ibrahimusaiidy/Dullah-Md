3017d2c18a8abcab34bd3ff2beda4230df21bd8c
Ibrahimusaiidy <saiidibrahim192@gmail.com>
2024-11-29 07:51:43 +0300

    Update alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -21,7 +21,7 @@ zokou(
         const {message , lien} = data;
 
 
-        var mode = "public";
+        var mode = "private";
         if ((s.MODE).toLocaleLowerCase() != "yes") {
             mode = "private";
         }
@@ -50,8 +50,8 @@ const date = moment().format('DD/MM/YYYY');
         zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
     }
     catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+        console.log("😛😛 Menu erreur " + e);
+        repondre("😛😛 Menu erreur " + e);
     }
 } 
 // Checking for .jpeg or .png
@@ -60,8 +60,8 @@ else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
         zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
     }
     catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+        console.log("😛😛 Menu erreur " + e);
+        repondre("😛😛 Menu erreur " + e);
     }
 } 
 else {
@@ -88,7 +88,7 @@ else {
     
 await addOrUpdateDataInAlive(texte , tlien)
 
-repondre(' Hello👋 , *DULLAH-MD BOT* is alive just like you my guy😜. ')
+repondre(' Hello👋 , *Mr b² 💥* is alive just like you my guy😜. ')
 
 }
     });
